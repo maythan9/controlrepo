4569288ad8a3f82e835a9cfd03c6808c6c64cb02
Ratanak <ratanak@localhost.localdomain>
2020-05-20 10:40:50 -0500

    edited from local machine

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
     ensure  => file,
-    content => 'This is a readme file',
+    content => 'This is a readme file. This line is written and commit from local machine',
     owner   => 'root',
   }
 }
