d43dea56911f24a083922c3e46ebabd6cea1b023
maythan9 <64553476+maythan9@users.noreply.github.com>
2020-05-21 10:45:20 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,4 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme file. This line is written and commit from local machine',
-    owner   => 'root',
   }
 
 node 'master.puppet.vm' {
