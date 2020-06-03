56e5c53060a4e1ac95b3937be1a7f0e34b6f03fa
Ratanak <ratanak@localhost.localdomain>
2020-06-03 10:47:34 -0500

    minecraft folder

---
+++ site/minecraft/init.pp
@@ -0,0 +1,29 @@
+#manifests-init.pp should always contain a class with the same name
+#as the module itself.
+
+class minecraft {
+	file {'/opt/minecraft':
+		ensure => directory,
+	}
+
+	file {'/opt/minecraft/server.jar':
+		ensure => file,
+		source => https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+	}
+
+	package {'java':
+		ensure => present,
+	}
+
+	file {'/opt/minecraft/eula.txt':
+		ensure  => file,
+		content => 'eula=true',
+	}
+
+	file {/'etc/sytemd/sytem/minecraft.service':
+		ensure  => file,
+		source  => 'puppet:///modules/minecraft/minecraft.service',
+                #source of Unit file which is created in github.
+	}
+
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
